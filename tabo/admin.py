30f296fd501a163add0b431719c476f8a177341b
Ansh Badaya <me.anshbadaya@gmail.com>
2023-12-05 18:23:31 +0530

    Added list per page

--- tabo/admin.py
+++ tabo/admin.py
@@ -2,14 +2,16 @@ from django.contrib import admin
 from .models import Processed,Scraped
 
 @admin.register(Processed)
-class StockAdmin(admin.ModelAdmin):
+class ProcessedAdmin(admin.ModelAdmin):
     search_fields = ['time']
     list_display = ['time', 'temperature']
     readonly_fields = ["time",'temperature']
+    list_per_page = 50
 
 
 @admin.register(Scraped)
-class StockAdmin(admin.ModelAdmin):
+class ScrapedAdmin(admin.ModelAdmin):
     search_fields = ['time']
     list_display = ['time', 'temperature']
     readonly_fields = ["time",'temperature']
+    list_per_page = 50
